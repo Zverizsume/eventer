baee764e7ce57dd32640dfef452c24afc6ae00ed
Zverizsume <zeliranje@gmail.com>
2024-03-18 10:57:23 +0100

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Providers } from './providers'
 import { montserrat, karla } from '@/utils/fonts'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
         <main>
           <Providers>
             {children}
+            <Analytics />
           </Providers>
         </main>
       </body>
